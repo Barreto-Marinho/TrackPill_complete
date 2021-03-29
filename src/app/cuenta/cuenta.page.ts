6a8865e3cd98e46eb173a42a29415f59915f1877
sebastian mariño morales <marinosebastian5@gmail.com>
2021-03-28 19:57:02 -0500

    Update cuenta.page.ts

--- src/app/cuenta/cuenta.page.ts
+++ src/app/cuenta/cuenta.page.ts
@@ -34,7 +34,6 @@ export class CuentaPage implements OnInit {
     if(estaverificado){
       this.router.navigate(['/folder/ ']);
     }else{
-      this.router.navigate(['/cuenta/']);
       this.Imprimir_error("El correo aun no esta verificado")
     }
   }
