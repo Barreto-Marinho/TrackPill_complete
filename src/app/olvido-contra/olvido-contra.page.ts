09df7ea05a72c7458f1cc10c26584a44fb257eb3
marinho14 <70497951+marinho14@users.noreply.github.com>
2021-03-30 16:21:03 -0500

    contaseña forgot

--- src/app/olvido-contra/olvido-contra.page.ts
+++ src/app/olvido-contra/olvido-contra.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-olvido-contra',
@@ -9,16 +10,39 @@ import { AuthService } from '../services/auth.service';
 })
 export class OlvidoContraPage implements OnInit {
 
-  constructor(private authSvc:AuthService, private router: Router) { }
+  constructor(private authSvc:AuthService, private router: Router,
+    private alertController: AlertController){ }
 
   ngOnInit() {
   }
 
   async Recuperar(email){
     try{
+      const user= await  this.authSvc.isEmailVerify(email.value);
+      if(user){
       await this.authSvc.resetPassword(email.value);
       this.router.navigate(['/cuenta']);
+    }else{
+      this.Imprimir_error("El correo no esta registrado")
+    }
     }
     catch(error){console.log('Error->',error)} 
+
+
   }
+
+  async Imprimir_error(texto){
+      const alert = await this.alertController.create({
+        cssClass: 'my-custom-class',
+        header: 'Alert',
+        subHeader: texto,
+        //message: texto,
+        buttons: ['OK']
+      });
+  
+      await alert.present();
   }
+
+  }
+
+
