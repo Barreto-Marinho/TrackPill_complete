9d97b31452987cc13373c58e56ca5277e8693d99
Andres <afelipebarretoj@javeriana.edu.co>
2021-04-08 17:16:15 -0500

    cambio en seguimiento

--- src/app/seguimiento/seguimiento.page.ts
+++ src/app/seguimiento/seguimiento.page.ts
@@ -6,10 +6,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./seguimiento.page.scss'],
 })
 export class SeguimientoPage implements OnInit {
-
+  public dias = "Lunes"
+  public cont_dia= 1
   constructor() { }
 
   ngOnInit() {
   }
+  Adelante(){
+    this.cont_dia= this.cont_dia+1;
+    if(this.cont_dia>6){
+      this.cont_dia=1;
+    }
+    this.impri_dia()
+  }
+
+  atras(){
+    this.cont_dia= this.cont_dia-1;
+    if(this.cont_dia<1){
+      this.cont_dia=7;
+    }
+    this.impri_dia()
+  }
+
+  impri_dia(){
+    switch(this.cont_dia) { 
+      case 1: { 
+           this.dias="Lunes"
+         break; 
+      } 
+      case 2: { 
+          this.dias="Martes"
+         break; 
+      } 
+      case 3: { 
+        this.dias="Miercoles"
+        break; 
+      } 
+      case 4: { 
+        this.dias="Jueves" 
+        break; 
+      }
+      case 5: { 
+        this.dias="Viernes" 
+        break; 
+      }  
+      case 6: { 
+        this.dias="Sabado" 
+        break; 
+      }case 7: { 
+        this.dias="Domingo"
+        break; 
+    }   
+   } 
+  }
+
 
 }
+
