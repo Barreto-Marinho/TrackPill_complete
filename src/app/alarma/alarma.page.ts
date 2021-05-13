067d0d994efc155759479e253c2436b870c8c676
sebastian mariño morales <marinosebastian5@gmail.com>
2021-05-13 10:58:19 -0500

    alarma

--- src/app/alarma/alarma.page.ts
+++ src/app/alarma/alarma.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { stringify } from '@angular/compiler/src/util';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController,ToastController } from '@ionic/angular';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-alarma',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alarma.page.scss'],
 })
 export class AlarmaPage implements OnInit {
+  public place_temperatura:string;
+  public place_humedad:string;
 
-  constructor() { }
+  constructor(private authSvc:AuthService, private router:Router,
+    private alertController: AlertController,
+    public toastController: ToastController) { }
 
   ngOnInit() {
   }
+
+  ionViewWillEnter(){
+    this.Imprimir_Temperatura();
+}
+
+ public Imprimir_Temperatura(){
+    this.place_temperatura= "30";
+    this.place_humedad= "20";
+}
+
+
+
+
 
 }
