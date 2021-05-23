19535382d79cb49d6a164c38c80f682496b98c60
sebastian mariño morales <marinosebastian5@gmail.com>
2021-05-22 19:13:53 -0500

    qr code :angel:

--- src/app/compart1/compart1.page.ts
+++ src/app/compart1/compart1.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController,ToastController } from '@ionic/angular';
 import { AuthService } from '../services/auth.service';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
 
 @Component({
   selector: 'app-compart1',
@@ -21,7 +22,8 @@ export class Compart1Page implements OnInit {
   public place_humedad_max= "Ingresa humedad maxima";
 
   constructor(private authSvc:AuthService,
-              private alertController: AlertController) { }
+              private alertController: AlertController,
+              private qrScanner: QRScanner) { }
 
   ngOnInit() {
   }
@@ -141,5 +143,33 @@ async Imprimir_error(texto){
     this.habilitar = false;
     this.isDisabled= true;
   }
+
+// Optionally request the permission early
+
+public leerqr(){
+this.qrScanner.prepare()
+  .then((status: QRScannerStatus) => {
+     if (status.authorized) {
+       // camera permission was granted
+
+
+       // start scanning
+       let scanSub = this.qrScanner.scan().subscribe((text: string) => {
+         console.log('Scanned something', text);
+
+         this.qrScanner.hide(); // hide camera preview
+         scanSub.unsubscribe(); // stop scanning
+       });
+
+     } else if (status.denied) {
+       // camera permission was permanently denied
+       // you must use QRScanner.openSettings() method to guide the user to the settings page
+       // then they can grant the permission from there
+     } else {
+       // permission was denied, but not permanently. You can ask for permission again at a later time.
+     }
+  })
+  .catch((e: any) => console.log('Error is', e));}
+
 
 }
